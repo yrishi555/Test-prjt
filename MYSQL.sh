a03d319f575bb78e14af29152d0194e611bd7e9d
yrishi555 <30583455+yrishi555@users.noreply.github.com>
2017-08-17 19:51:21 +0530

    Update MYSQL.sh

--- MYSQL.sh
+++ MYSQL.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';FLUSH PRIVILEGES;"  > /home/bitnami/mysql-init
 /opt/bitnami/ctlscript.sh stop mysql
 /opt/bitnami/mysql/bin/mysqld_safe --pid-file=/opt/bitnami/mysql/data/mysqld.pid --datadir=/opt/bitnami/mysql/data --init-file=/home/bitnami/mysql-init 2> /dev/null &
