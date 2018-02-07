742244590c0c903ab497cc31ba4fbc3eed83d9ba
yrishi555 <30583455+yrishi555@users.noreply.github.com>
2018-02-07 16:22:21 +0530

    Create azvmss.sh

---
+++ azvmss.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo -i
+apt-get update
+apt-get install haproxy
+sed -i 's,^\(ENABLED=\).*,\1'1',' /etc/default/haproxy
+echo "frontend haproxynode \nbind *:80 \nmode http \ndefault_backend backendnodes \nbackend backendnodes \nbalance roundrobin \nserver node2 10.1.0.5:80 check" >> /etc/haproxy/haproxy.cfg
+service haproxy restart
